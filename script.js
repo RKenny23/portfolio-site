33ce81b60ebb58c2d73fb3c9926f4de94f24254d
RKenny23 <rkenny238@hotmail.com>
2022-03-25 15:25:42 -0400

    added text, flipped the order of sections.

--- script.js
+++ script.js
@@ -31,3 +31,10 @@ function showSlides(n) {
   slides[slideIndex - 1].style.display = "block";
   dots[slideIndex - 1].className += " active";
 }
+
+const toggleButton = document.getElementsByClassName('nav-toggler')[0];
+const navbarLinks = document.getElementsByClassName('nav-links')[0];
+
+toggleButton.addEventListener('click', () => {
+  navbarLinks.classList.toggle('active')
+});
